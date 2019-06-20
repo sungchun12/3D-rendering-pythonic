6b96f657f5d38e1a0ef9b522361e79b3be61c00e
sungchun12 <sungwonchung3@gmail.com>
2019-06-20 00:40:14 -0500

    updated for more scratchpad

--- scratch_pad.sh
+++ scratch_pad.sh
@@ -1,5 +1,10 @@
 
 # install libgl for pyqt dependency
-apt update
+apt-get update
 apt install libgl1-mesa-glx
 apt install libqxcb
+
+# get this working for pyaudio
+apt-get install portaudio19-dev
+
+apt-get install eglfs, linuxfb, minimal, minimalegl, offscreen, vnc, wayland-egl, wayland, wayland-xcomposite-egl, wayland-xcomposite-glx, webgl, xcb.
